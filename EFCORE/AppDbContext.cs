4b70110f168bb2f3961bf072833b87f64438ddaf
aysunfarhadli <aysunfarhadli4@gmail.com>
2025-04-25 13:36:02 +0400

    first commit

---
+++ EFCORE/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCORE
+{
+    internal class AppDbContext:DbContext
+    {
+  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+
+
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("server=DESKTOP-GTVND9D;database=AB109_EF;Trusted_connection = true;Encrypt= false; TRUSTSERVERCERTIFICATE=true;");
+
+
+        }
+
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<Student> Students { get; set; }
+    }
+
+
+}
---
+++ EFCORE/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace EFCORE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new AppDbContext())
+            {
+                AddData(context);
+                RemoveData(context);
+                UpdateData(context);
+            }
+        }
+
+        static void AddData(AppDbContext context)
+        {
+            var group = new Group { Name = "Mathematics" };
+            var student1 = new Student { Name = "Aysun Aliyeva", Group = group };
+            var student2 = new Student { Name = "Ayten Farhadli", Group = group };
+
+            context.Groups.Add(group);
+            context.Students.Add(student1);
+            context.Students.Add(student2);
+            context.SaveChanges();
+        }
+
+        static void RemoveData(AppDbContext context)
+        {
+            var student = context.Students.FirstOrDefault(s => s.Name == "Aysun Aliyeva");
+            if (student != null)
+            {
+                context.Students.Remove(student);
+                context.SaveChanges();
+            }
+        }
+
+        static void UpdateData(AppDbContext context)
+        {
+            var student = context.Students.FirstOrDefault(s => s.Name == "Ayten Farhadli");
+            if (student != null)
+            {
+                student.Name = "Ayten Aliyeva";
+                context.SaveChanges();
+            }
+        }
+    }
+}
